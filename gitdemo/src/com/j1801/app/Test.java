1068e1c22c0a7e0af6be30a8cb6d7667d8568ad5
ALIMUS\alimus <las12503050@sina.com>
2018-06-21 16:26:33 +0800

    添加了一个test类

---
+++ gitdemo/src/com/j1801/app/Test.java
@@ -0,0 +1,17 @@
+package com.j1801.app;
+
+public class Test {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+}
